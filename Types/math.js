a1b5bd077179a551c47f81a5f3567814a2703e73
Pedro Araujo <paraujo@emh.br>
2022-06-27 16:56:10 -0300

    até aula 3.8

---
+++ Types/math.js
@@ -0,0 +1,28 @@
+//arredondandos numeros decimais
+
+//arredondando para cima:
+const v1 = Math.ceil(3.5);
+console.log(v1);
+
+//arredondando para baixo:
+const v2 = Math.floor(3.5);
+console.log(v2);
+
+//usndo o 'round' ele arredonda para o valor mais proximo:
+const v3 = Math.round(3.6);
+
+
+//calculando raiz quadrada
+const v4 = Math.sqrt(64);
+console.log(v4);
+
+//fazendo a potenciação
+const v5 = Math.pow(3,2); //3 é o numero que vai ser elevado e 2 a potencia de elevação (3 elevado a 2)
+console.log(v5);
+
+//valor MAXIMO e valor MINIMO de uma sequencia
+const v6 = Math.max(3,2,7,1,4,10);
+console.log(v6);
+
+const v7 = Math.min(3,2,7,1,4,10);
+console.log(v7);
---
+++ Types/type.js
@@ -0,0 +1,11 @@
+//typeof mostra qual o tipo de dado da variavel (string, number, bool, etc...)
+
+const v1 = 5;
+const v2 = 3.65;
+const v3 = "pedro";
+const v4 = true;
+
+console.log(typeof(v1));
+console.log(typeof(v2)); //-> no JavaScript, não existe diferença no tipo de dado entre num inteiro e decimal, tudo é considerado number.
+console.log(typeof(v3));
+console.log(typeof(v4));
