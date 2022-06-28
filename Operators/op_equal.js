92f97d90b43d448b09bea591d3965d603e0d7a83
Pedro Araujo <paraujo@emh.br>
2022-06-28 17:04:34 -0300

    até aula 4.5

---
+++ Operators/op_equal.js
@@ -0,0 +1,15 @@
+console.log(1 === "1"); //mais rigido/preciso na comparação.
+console.log(1 === true);
+console.log(null === undefined);
+
+console.log(1 == "1"); //faz a comparação com menos rigidez, tenta fazer alguma conversão.
+console.log(1 == true);
+console.log(null == undefined);
+
+console.log(1 !== "1");
+console.log(1 !== true);
+console.log(null !== undefined);
+
+console.log(1 != "1");
+console.log(1 != true);
+console.log(null != undefined);
