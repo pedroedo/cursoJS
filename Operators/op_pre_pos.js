2fb615bddd96a4cbddaf1467a2ffc322e1b3f822
Pedro Araujo <paraujo@emh.br>
2022-06-29 09:29:29 -0300

    unidade 4 finalizada

---
+++ Operators/op_pre_pos.js
@@ -0,0 +1,7 @@
+// O ++n deve ser usado quando não for em uma expressão/conta.
+// O n++ pode ser usado em uma expressão/conta
+let n = 10;
+let a = 5 + n++;
+
+console.log("a -> " + a);
+console.log("n -> " + n);
