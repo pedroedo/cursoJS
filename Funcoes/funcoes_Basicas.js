ab36c021b97378e4d0bc1f0bc0920b2923e1f5d9
Pedro Araujo <paraujo@emh.br>
2022-07-07 13:54:50 -0300

    Unidade 8 finalizada

---
+++ Funcoes/funcoes_Basicas.js
@@ -0,0 +1,7 @@
+console.log("Inicio");
+dizerBomDia();
+console.log("Fim");
+
+function dizerBomDia(){
+    console.log("Bom dia aiaiai!");
+}
---
+++ Funcoes/parametros.js
@@ -0,0 +1,8 @@
+//Os parametros sao passados dentro dos parenteses da função.
+
+sum(2, 6); //chamando a função passando os valores que os parametros vao receber.
+
+function sum(a, b){
+    const r = a + b;
+    console.log(r);
+}
